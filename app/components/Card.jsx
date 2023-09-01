753409071f3f230d03dac2524057eca9a0b79d2a
theisolatedmonk <96rahulseth@gmail.com>
2023-09-01 22:00:50 +0530

    initial commit

---
+++ app/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Card () {
+  return (
+    <div className='w-full h-full bg-blue-100  '>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 '></div>
+    </div>
+  )
+}
---
+++ app/components/Fullpage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Card from '@/app/components/Card'
+import Image from 'next/image'
+import avtar from '@/public/images/image-jeremy.png'
+export default function Fullpage () {
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 w-full bg-[hsl(226,43%,10%)] py-40 px-20 gap-x-4 h-full justify-center items-center'>
+      <div className='rounded-lg bg-[hsl(235,46%,20%)]  w-full sm:w-40 h-full'>
+        <div className='flex sm:flex-col p-4 bg-[hsl(235,45%,61%)] h-[60%] rounded-lg gap-4'>
+          <div className='rounded-full'>
+            <Image
+              src={avtar}
+              height={40}
+              width={40}
+              className='ring-1 rounded-full ring-white'
+            />
+          </div>
+          <div className='flex flex-col'>
+            <p className='text-xs '>Report for</p>
+            <p className='text-xl '>Jeremy Robson</p>
+          </div>
+        </div>
+        <div className='grid p-4 sm:grid-cols-1 grid-cols-3'>
+          <p className=''>Daily</p>
+          <p className=''>Weekly</p>
+          <p className=''>Monthly</p>
+        </div>
+      </div>
+      <Card />
+    </div>
+  )
+}
